93c7cbc7ccee2c019d4709df395dddac68abddb7
Anchen <li.anchen.au@gmail.com>
2023-10-25 02:25:03 +1100

    chore: make the param more readable

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,32 @@ use model::TextGeneration;
 
 fn main() -> Result<()> {
     let tokenizer_filename = PathBuf::from("./models/tokenizer.json");
-    let filenames = vec![PathBuf::from("./models/model-q4k.gguf")];
+    let model_name = PathBuf::from("./models/model-q4k.gguf");
     let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
 
     let config = Config::config_7b_v0_1(true);
-    let filename = &filenames[0];
-    let vb = candle_transformers::quantized_var_builder::VarBuilder::from_gguf(filename)?;
+    let vb = candle_transformers::quantized_var_builder::VarBuilder::from_gguf(model_name)?;
     let model = QMistral::new(&config, vb)?;
     let device = Device::Cpu;
+
     let seed = 299792458;
+    let temperature = Some(0.7);
+    let top_p = Some(0.95);
+    let repeat_penalty = 1.1;
+    let repeat_last_n = 64;
+    let sample_len = 400;
 
     let mut pipeline = TextGeneration::new(
         model,
         tokenizer,
         seed,
-        Some(0.7),
-        Some(0.95),
-        1.1,
-        64,
+        temperature,
+        top_p,
+        repeat_penalty,
+        repeat_last_n,
         &device,
     );
-    let res = pipeline.run("hello world".into(), 400)?;
+    let res = pipeline.run("hello world".into(), sample_len)?;
     println!("result: {:?}", res.join(""));
     Ok(())
 }
